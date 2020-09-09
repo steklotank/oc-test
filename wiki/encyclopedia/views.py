b5432199b5755b04ca755edfe88a8fb0ece3d0a4
Sergey Kabanen <steklotank@gmail.com>
2020-09-09 20:25:38 +0300

    add a link

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -7,11 +7,17 @@ from . import util
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries() 
     })
 
 def title(request, title):
 	return render (request,"encyclopedia/title.html", {
 		"title": title, 
         "article": markdown2.markdown(util.get_entry(title))
+	})
+
+def editor(request, title):
+	return render (request,"encyclopedia/title.html", {
+		"title": title, 
+        "article": util.get_entry(title)
 	})
