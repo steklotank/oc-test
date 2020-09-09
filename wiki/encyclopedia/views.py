aa03a69a07ff4a0ac10d10a11cc1275d30133852
Sergey Kabanen <steklotank@gmail.com>
2020-09-09 19:40:36 +0300

    render a single page + read and convert markdown

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+import markdown2
 from . import util
+
+
 
 
 def index(request):
@@ -11,5 +13,5 @@ def index(request):
 def title(request, title):
 	return render (request,"encyclopedia/title.html", {
 		"title": title, 
-        "article": util.get_entry(title)
+        "article": markdown2.markdown(util.get_entry(title))
 	})
